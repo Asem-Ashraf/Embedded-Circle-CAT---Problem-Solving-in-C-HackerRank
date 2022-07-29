7a4c8fc882f70e58eda7b1f6b87dfe69b95b0af2
Asem Ashraf <73125888+LOGIC-ar@users.noreply.github.com>
2022-07-30 01:12:34 +0200

    organized the files

---
+++ C/a. Conditional Statements in C.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+int main(){
+    int s;
+    scanf("%d",&s);
+    if(s==1) {
+       printf("one"); 
+    }
+    else if(s==2) {
+       printf("two"); 
+    }
+    else if(s==3) {
+        printf("three"); 
+    }
+    else if(s==4) {
+        printf("four"); 
+    }
+    else if(s==5) {
+        printf("five"); 
+    }
+    else if(s==6) {
+        printf("six"); 
+    }
+    else if(s==7) {
+        printf("seven"); 
+    }
+     else if(s==8) {
+        printf("eight"); 
+    }
+     else if(s==9) {
+        printf("nine"); 
+    }
+    else {
+        printf("Greater than 9"); 
+    }
+}
